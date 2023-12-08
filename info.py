105243922a303adb6b3bce6514b5400ac342e3f5
Enayat7800 <153251094+Enayat7800@users.noreply.github.com>
2023-12-08 08:30:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '28150346'))
+API_HASH = environ.get('API_HASH', '426f0d0a1da02dea8fb71cb0bd3ab7e1')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6913569613:AAGCOpKh45PL0_qHBNU41CAMUrDLq_IDcgk")
 
 #Port
 PORT = environ.get("PORT", "8080")
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ronagi5556:enayatali@cluster0.dtbgsjx.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
